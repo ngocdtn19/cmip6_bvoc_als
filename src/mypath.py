84c523eda58cb8231d721ada8aeaed470396a6c9
ngoc <ngocdtn19@gmail.com>
2023-03-19 00:42:20 +0900

    fix yml and path

--- src/mypath.py
+++ src/mypath.py
@@ -2,9 +2,9 @@ import glob
 import os
 from const import *
 
-DATA_DIR = "../data/"
-VAR_DIR = "../data/var/"
-AXL_DIR = "../data/axl/"
+DATA_DIR = "/mnt/dg3/ngoc/cmip6_bvoc_als/data"
+VAR_DIR = os.path.join(DATA_DIR, "var")
+AXL_DIR = os.path.join(DATA_DIR, "axl")
 LIST_ATTR = [attr.split("\\")[-1] for attr in glob.glob(os.path.join(VAR_DIR, "*"))]
 
 ISOP_LIST = glob.glob(os.path.join(VAR_DIR, "emiisop", "*.nc"))
